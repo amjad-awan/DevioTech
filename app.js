6ec43c25f61bab97ba313893a3a3121174a8c11c
amjad-awan <73718759+amjad-awan@users.noreply.github.com>
2022-02-20 02:53:28 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ window.addEventListener("DOMContentLoaded",function(){
 sr.reveal('.animate-left', {
     origin:'left',
     duration:1000,
-    distance:'25rem',
+    distance:'10rem',
        reset: true,
     delay:300
 });
@@ -30,21 +30,21 @@ window.sr=ScrollReveal();
 sr.reveal('.animate-right', {
     origin:'right',
     duration:1000,
-    distance:'25rem',
+    distance:'10rem',
        reset: true,
     delay:300
 });
 sr.reveal('.animate-bottom', {
     origin:'bottom',
     duration:1000,
-    distance:'25rem',
+    distance:'10rem',
        reset: true,
     delay:300
 });
 sr.reveal('.animate-top', {
     origin:'top',
     duration:1000,
-    distance:'25rem',
+    distance:'10rem',
        reset: true,
     delay:600
 });
